68b56b77c869466997936e41c96526acad94f26a
Matt Moore <mmoore@groupon.com>
2013-04-21 16:56:57 -0500

    require all models

--- lib/models.rb
+++ lib/models.rb
@@ -4,5 +4,6 @@ module Rushour
     require_relative 'models/commute'
     require_relative 'models/commute_template'
     require_relative 'models/place'
+    require_relative 'models/captured_email'
   end
 end
