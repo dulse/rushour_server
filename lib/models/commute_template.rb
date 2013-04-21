1819b928a441f2c439549b877f6ca28de8802978
Matt Moore <mmoore@groupon.com>
2013-04-20 22:21:37 -0500

    fix model association

--- lib/models/commute_template.rb
+++ lib/models/commute_template.rb
@@ -2,8 +2,8 @@ module Rushour
   module Models
     class CommuteTemplate < ActiveRecord::Base
       belongs_to :user
-      has_one :start_place, foreign_key: 'start_place_id', class: Place
-      has_one :end_place, foreign_key: 'end_place_id', class: Place
+      has_one :start_place, foreign_key: 'start_place_id', class: 'Rushour::Models::Place'
+      has_one :end_place, foreign_key: 'end_place_id', class: 'Rushour::Models::Place'
     end
   end
 end
